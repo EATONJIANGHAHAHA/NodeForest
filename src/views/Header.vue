451fc938d5adf30c4c3f2de2ffa6f46e7ca79d82
eaton <12193583@student.uts.edu.au>
2018-08-19 18:02:27 +1000

    implementing home page

---
+++ src/views/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="header">
+        <img src="../../public/logo.png" height="30px" width="30px">
+        <div id="right-section" class="right-section">
+            <router-link to="/register" id="register">Register</router-link>
+            <router-link to="/about" id="about">About</router-link>
+            <router-link to="/login" id="login">Login</router-link>
+            <router-link to="/home" id="home">Home</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Button from "muse-ui/es5/internal/mixins/button"
+
+    export default {
+        name: "Header",
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    #logo {
+        float: left
+    }
+    #right-section {
+        float: right;
+    }
+    #header {
+        padding:5px;
+    }
+    #register {
+        margin: 5px;
+    }
+    #about {
+        margin: 5px;
+    }
+    #login {
+        margin: 5px;
+    }
+    #home {
+        margin: 5px;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <Header/>
+        <div id="tree-number">
+            <p>Tree No. {{treeNumber}}</p>
+        </div>
+        <div id="middle-section">
+            <div id="picture-section">
+                <img src="../../public/tree.jpg" height="200px" width="200px"/>
+                <p>Updated at: {{date}}th, {{month}}, {{year}}</p>
+            </div>
+            <div id="tree-info">
+                <p>Height: {{height}}cm</p><br/>
+                <p>Health: {{health}}</p><br/>
+                <p>Location: {{location}}</p><br/>
+                <p>view history photos</p> <!--this should be a route instead-->
+            </div>
+        </div>
+        <div id="lower-section">
+            <!--<button>Change Tree Name</button>
+            <button>Request for a Leaf</button>
+            <button>Request for a Postcard</button>-->
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from "../views/Header"
+
+    export default {
+        name: "Home",
+
+        data() {
+            return {
+                treeNumber: 123456,
+                date: 32,
+                month: 'Jun',
+                year: 2020
+            }
+        },
+
+        components: {
+            Header
+        }
+    }
+</script>
+
+<style scoped>
+    #tree-number {
+        padding: 20px 20px 20px 100px;
+        font-size: 50px;
+    }
+
+    #picture-section {
+        float: left;
+        margin: 0 auto;
+    }
+
+    #tree-info {
+        float: right;
+        margin: 0 auto;
+    }
+
+    #middle-section {
+        text-align: center;
+        align-items: center;
+        margin: 50px;
+        margin-left: 500px;
+        margin-right: 500px;
+    }
+    #lower-section {
+
+    }
+</style>
