d9eaaff8b72c3f4e6f5bf59027046405963a0e1d
eaton <12193583@student.uts.edu.au>
2018-08-23 12:30:47 +1000

    Refactoring Login page, using Muse-UI, Login page is now able to select Login account type.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,61 +1,82 @@
 <template>
     <div>
         <Header/>
-        <div id="main">
+        <mu-container>
             <h1>Welcome</H1>
             <p v-if="!hide">descriptions</p>
-            <form v-if="!hide">
-                USERNAME:<br>
-                <input type="text" name="username" v-model="userName"/>
-                <br>
-                PASSWORD:<br>
-                <input type="text" name="password" v-model="passWord"/>
-                <br><br>
-                <router-link to="/home" id="home">Log in as user</router-link>
-                <router-link to="/treemanagement" id="staff">Log in as staff</router-link>
-                <mu-container class="button-wrapper">
-                    <mu-button color="primary">Primary</mu-button>
-                    <mu-button color="secondary">Secondary</mu-button>
-                    <mu-button color="success">Success</mu-button>
-                    <mu-button color="warning">Warning</mu-button>
-                    <mu-button color="info">Info</mu-button>
-                    <mu-button color="error">Error</mu-button>
-                    <mu-button disabled>disabled</mu-button>
-                    <mu-container>
-
-                    </mu-container></mu-container>
-            </form>
-        </div>
+            <mu-form :model="form" class="mu-login-form" :label-position="labelPosition" label-width="100">
+                <mu-form-item prop="input" label="Username" >
+                    <mu-text-field v-model="form.userName"></mu-text-field>
+                </mu-form-item>
+                <mu-form-item prop="input" label="Password" >
+                    <mu-text-field v-model="form.passWord"></mu-text-field>
+                </mu-form-item>
+                <mu-form-item prop="select" label="Login as: ">
+                    <mu-select v-model="form.select">
+                        <mu-option v-for="option, index in options" :key="option" :label="option"
+                                   :value="option"></mu-option>
+                    </mu-select>
+                </mu-form-item>
+            </mu-form>
+            <mu-button @click="check">Login</mu-button>
+        </mu-container>
     </div>
 </template>
 
 <script>
     import Header from '../components/Header'
+    import Router from '../router'
 
     export default {
         name: "Login",
         data() {
             return {
-                userName: '',
-                passWord: '',
-                hide: false,
+                usernameRules: [
+                    { validate: (val) => !!val, message: 'User name is required'},
+                    { validate: (val) => val.length >= 3, message: 'User name has to be longer than 3 characters'}
+                ],
+                passwordRules: [
+                    { validate: (val) => !!val, message: 'Password is required'},
+                    { validate: (val) => val.length >= 3 && val.length <= 10, message: 'Password name has to be longer than 3 characters'}
+                ],
+                options: [
+                    'User', 'Staff'
+                ],
+                labelPosition: 'top',
+                form: {
+                    userName: '',
+                    passWord: '',
+                }
+
             }
         },
         components: {
             Header
         },
         methods: {
-            checkUser() {
-                if (this.userName === '123' && this.passWord === '456') {
-                    this.hide = true
+            check() {
+                if (this.form.userName === '123' && this.form.passWord === '456') {
+                    if (this.form.select === 'User') {
+                        Router.push('home')
+                        this.clear()
+                    }
+                    else {
+                        //TODO: jump to staff main page.
+                    }
                 }
-            }
+            },
+
+            clear() {
+                this.form.userName = '';
+                this.form.passWord = '';
+            },
         }
     }
 </script>
 
 <style scoped>
-    #main {
-        text-align: center;
+    .mu-login-form {
+        width: 100%;
+        max-width: 460px;
     }
 </style>
