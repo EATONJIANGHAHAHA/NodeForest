d6fc340243f882823c1b0cf1b4263e965e330022
leighton070103 <leighton070103@gmail.com>
2018-08-23 17:24:08 +1000

    Updated register page.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,29 +1,50 @@
 <template>
     <div>
         <Header/>
-        <p>Register</p>
-        <div id="main">
-            <h1>Welcome</H1>
-            <form v-if="!hide">
-                USERNAME:<br>
-                <input type="text" name="username" v-model="userName"/>
-                <br>
-                PASSWORD:<br>
-                <input type="text" name="password" v-model="passWord"/>
-                <br>Email: <br>
-                <input type="text" name="email"/>
-            </form>
-        </div>
+        <mu-container>
+            <h1>Register</H1>
+            <mu-form :model="form" class="mu-login-form" :label-position="labelPosition" label-width="100">
+                <mu-form-item prop="input" label="Username">
+                    <mu-text-field v-model="form.userName"></mu-text-field>
+                </mu-form-item>
+                <mu-form-item prop="input" label="Password">
+                    <mu-text-field v-model="form.passWord"></mu-text-field>
+                </mu-form-item>
+                <mu-form-item prop="input" label="Email">
+                    <mu-text-field v-model="form.email"></mu-text-field>
+                </mu-form-item>
+            </mu-form>
+            <mu-button @click="validate">Register</mu-button>
+        </mu-container>
     </div>
 </template>
 
 <script>
     import Header from '../components/Header'
+    import Router from '../router'
+    import store from "../store";
 
     export default {
         name: "Register",
+        data() {
+            return {
+                form: {
+                    userName: '',
+                    passWord: '',
+                    email: ''
+                }
+            }
+        },
         components: {
             Header,
+        },
+        methods: {
+          validate(){
+              if(this.form.userName != '') {
+                  Router.push('home')
+                  store.commit("SET_USER", userName)
+              }
+          }
         }
     }
 </script>
