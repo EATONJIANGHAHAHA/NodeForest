9a1fd0c049fb583d80cbd1df8a537eb9d0555125
EATONJIANGHAHAHA <eatonjiangtonlei@gmail.com>
2018-10-08 17:34:13 +1100

    vue are checking session each time it starts and created.

--- src/views/StaffHome.vue
+++ src/views/StaffHome.vue
@@ -2,7 +2,7 @@
     <mu-container style="padding: 20px">
         <mu-expansion-panel>
             <div slot="header">Trees</div>
-            <TreeInfo id="tree-info-list" v-for="tree in trees" v-bind="tree" :key="tree.treeId"></TreeInfo>
+            <TreeInfo class="treeInfo" id="tree-info-list" v-for="tree in trees" v-bind="tree" :key="tree.treeId"></TreeInfo>
         </mu-expansion-panel>
         <mu-expansion-panel>
             <div slot="header">Tree Applications</div>
@@ -48,5 +48,8 @@
 </script>
 
 <style scoped>
-
+    .treeInfo{
+        display: inline-block;
+        padding: 10px;
+    }
 </style>
